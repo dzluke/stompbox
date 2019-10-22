71dd6dab477d59f7362e5493a47f1635c0fdb069
dz.luke <dz.luke@gmail.com>
2019-10-22 11:21:34 -0700

    Able to send UDP packets over wifi

---
+++ connect_over_wifi/connect_over_wifi.ino
@@ -0,0 +1,82 @@
+
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+#include <OSCBundle.h>
+#include <OSCData.h>
+
+char ssid[] = "DFO";
+char pass[] = "payasparab";
+
+IPAddress dest_ip(192, 168, 1, 110);
+const int dest_port = 7000;
+const int localPort = 7000; //used for Udp.begin() which I think listens
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP Udp;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  //Setup pins
+//  for (int i = 0; i < 24; i++) {
+//    //pin 13 has a built-in resistor on its LED which would require an additional resistor.  Lets leave it out
+//    if (i != 12 && i != 13) { 
+//      //all pins in pullup mode (inverts output but simplifies wiring)
+//      pinMode(i, INPUT_PULLUP);
+//    }
+//  }
+
+  //Setup WiFi
+  Serial.begin(115200);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+
+  int g=0;
+  Serial.print("WiFi Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  Serial.println("Starting UDP");
+  Udp.begin(localPort);
+  Serial.print("Local port: ");
+#ifdef ESP32
+  Serial.println(localPort);
+#else
+  Serial.println(Udp.localPort());
+#endif
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Port: ");
+  Serial.println(localPort);
+}
+
+int i = 0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  OSCMessage msg("/test");
+  Serial.printf("Sending packet %d\n", i);
+  msg.add(i);
+  if (Udp.beginPacket(dest_ip, dest_port) == 0) {
+    Serial.println("ERROR: beginPacket failed");
+  }
+  msg.send(Udp);
+  Udp.endPacket();
+  msg.empty();
+  i++;
+  delay(2000);
+  
+  
+  
+
+}
