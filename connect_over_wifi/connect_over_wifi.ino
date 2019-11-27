2230e40e7d2f04d8848c9e8e0b6a9395ad3b9de4
dz.luke <dz.luke@gmail.com>
2019-11-26 16:24:44 -0800

    Use calibration values to map analog input

--- connect_over_wifi/connect_over_wifi.ino
+++ connect_over_wifi/connect_over_wifi.ino
@@ -10,10 +10,13 @@
 
 #define PIN_MAX_VALUE 4095
 
-char ssid[] = "DFO";
-char pass[] = "payasparab";
+// Calibration time in ms 
+#define CALIBRATION_TIME 10000
 
-IPAddress dest_ip(192, 168, 1, 110);
+char ssid[] = "adkbb";
+char pass[] = "******";
+
+IPAddress dest_ip(192, 168, 99, 105);
 const int dest_port = 7000;
 const int localPort = 7000; //used for Udp.begin() which listens
 
@@ -21,9 +24,12 @@ int POT_PIN = 34;
 int pot_val = 0;
 
 bool calibration = false;
-
 int digital_initial_state[NUM_DIGITAL_PINS];
-int analog_ranges[NUM_ANALOG_PINS];
+int analog_max[NUM_ANALOG_PINS];
+int analog_min[NUM_ANALOG_PINS];
+unsigned long calibration_end_time = -1;
+
+bool debug = true;
 
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP Udp;
@@ -64,6 +70,7 @@ void setup() {
   Serial.println(localPort);
 }
 
+
 /* Expect OSC message to addr /calibrate to be a 0 or 1 */
 void calibrate(OSCMessage &msg) {
   calibration = msg.getInt(0);
@@ -72,6 +79,7 @@ void calibrate(OSCMessage &msg) {
 
   // Calibrate all digital pins
   if (calibration) {
+    calibration_end_time = millis() + CALIBRATION_TIME;
     for (int i = 0; i < NUM_DIGITAL_PINS; i++) {
       digital_initial_state[i] = digitalRead(i);
     }
@@ -80,13 +88,35 @@ void calibrate(OSCMessage &msg) {
 }
 
 
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  // potentiometer code:
+//  potentiometer code:
 //  pot_val = analogRead(POT_PIN);
 //  Serial.println(pot_val);
 //  delay(1000);
+
+  unsigned long curr_time = millis();
+  if (calibration) {
+    if (curr_time < calibration_end_time) {
+      for (int i = 0; i < NUM_ANALOG_PINS; i++) {
+        int val = analogRead(i);
+        analog_min[i] = min(val, analog_min[i]);
+        analog_max[i] = max(val, analog_max[i]);
+      }
+    } else {
+      //Calibration is over
+      calibration = false;
+      if (debug) {
+        Serial.println("Calibration has ended. Min/max Values:");
+        for (int i = 0; i < NUM_ANALOG_PINS; i++) {
+          Serial.println(i);
+          Serial.print("Min: ");
+          Serial.println(analog_min[i]);
+          Serial.print("Max: ");
+          Serial.println(analog_max[i]);
+        }
+      }
+    }
+  }
 
   
   OSCBundle bndl_out;
@@ -100,25 +130,32 @@ void loop() {
       bndl_in.fill(Udp.read());
     }
     if (!bndl_in.hasError()) {
-//      Serial.println("Packet has no error");
       bndl_in.dispatch("/calibrate", calibrate);
     } else {
-//      Serial.println("Packet has error");
+      Serial.println("Packet has error");
     }
   }
   
-  
+  int val;
   for (int i = 0; i < NUM_DIGITAL_PINS; i++) {
     sprintf(addr, "/digital/%d", i);
-    int num = pot_val;
-//    printf("Sending %s : %d\n", addr, num);
-    bndl_out.add(addr).add(num);
+    val = digitalRead(i); 
+    val = val ^ digital_initial_state[i]; //XOR the reading with the initial reading of the pin
+    if (debug) {
+      val = pot_val;
+//      printf("Sending %s : %d\n", addr, val);
+    }
+    bndl_out.add(addr).add(val);
   }
   for (int i = 0; i < NUM_ANALOG_PINS; i++) {
     sprintf(addr, "/analog/%d", i);
-    int num = random(50);
-//    printf("Sending %s : %d\n", addr, num);
-    bndl_out.add(addr).add(num);
+    val = analogRead(i);
+    val = map(val, analog_min[i], analog_max[i], 0, PIN_MAX_VALUE); //Map the reading based on calibration
+    if (debug) {
+      val = random(50);
+//      printf("Sending %s : %d\n", addr, val);
+    }
+    bndl_out.add(addr).add(val);
   }
   if (Udp.beginPacket(dest_ip, dest_port) == 0) {
     Serial.println("ERROR: beginPacket failed");
